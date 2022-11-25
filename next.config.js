48c3e506d5a2675b4716929ec8ad098fa60b3fc7
LEECH <71697577+LEECHANHYUNG@users.noreply.github.com>
2022-11-25 11:28:59 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,41 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
+module.exports = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
+module.exports = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        kakaokey: 'dc629edb6c7c6ce0791c0f11c9b5d459',
+        signIn:
+          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDo8HpxKm27rkojUDl57ZSffSw0LR-t4p8',
+        SECRET: 'mysecretofnextjsnextauth',
+      },
+    };
+  }
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      env: {
+        kakaokey: 'dc629edb6c7c6ce0791c0f11c9b5d459',
+        signIn: '/auth/signin',
+        signUp: '/auth/signup',
+        myPage: '/mypage',
+        refresh: '/auth/refresh',
+        SECRET: 'mysecretofnextjsnextauth',
+      },
+    };
+  }
+  return {
+    env: {
+      kakaokey: 'dc629edb6c7c6ce0791c0f11c9b5d459',
+      FIREBASE_API_KEY: 'AIzaSyDo8HpxKm27rkojUDl57ZSffSw0LR-t4p8',
+      SECRET: 'mysecretofnextjsnextauth',
+    },
+  };
+};

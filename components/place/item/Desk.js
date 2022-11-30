bab70f885b8dd55a4c5f9b06c26c0a2e5e598886
LEECHANHYUNG <lsj721538@gmail.com>
2022-11-30 14:29:12 +0900

    refactor(css): refactor item css styling

--- components/place/item/Desk.js
+++ components/place/item/Desk.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  width: 100%;
+import Card from '../../ui/Card';
+const Wrapper = styled(Card)`
+  width: 90%;
   height: 80px;
   padding: 0 20px;
   display: flex;
   justify-content: space-around;
   align-items: center;
   margin-top: 20px;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #999;
+  padding: 10px;
+  border: 2px solid #6a9eff;
   & .description {
     width: 40%;
     font-size: 13px;
@@ -24,6 +24,16 @@ const Wrapper = styled.div`
     font-size: 1.3rem;
     color: #6a9eff;
   }
+  @media screen and (max-width: 1430px) {
+    & .description p {
+      display: none;
+    }
+  }
+  @media screen and (max-width: 1170px) {
+    & .description p {
+      display: block;
+    }
+  }
 `;
 const Desk = ({ price }) => {
   return (
@@ -33,7 +43,7 @@ const Desk = ({ price }) => {
         <p>1인이 이용하기에 적합한 상품입니다.</p>
       </div>
       <div className="price">
-        1시간 {Number(price).toLocaleString('ko-KR')} ~{' '}
+        1시간 {Number(price).toLocaleString('ko-KR')} ~
       </div>
     </Wrapper>
   );
--- components/place/item/Office.js
+++ components/place/item/Office.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Card from '../../ui/Card';
 
-const Wrapper = styled.div`
-  width: 100%;
+const Wrapper = styled(Card)`
+  width: 90%;
   height: 80px;
   padding: 0 20px;
   display: flex;
   justify-content: space-around;
   align-items: center;
   margin-top: 20px;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #999;
+  padding: 20px;
+  border: 2px solid #6a9eff;
   & .description {
     width: 40%;
     font-size: 13px;
@@ -23,6 +24,16 @@ const Wrapper = styled.div`
   & .price {
     font-size: 1.3rem;
     color: #6a9eff;
+  }
+  @media screen and (max-width: 1430px) {
+    & .description p {
+      display: none;
+    }
+  }
+  @media screen and (max-width: 1170px) {
+    & .description p {
+      display: block;
+    }
   }
 `;
 const Office = ({ price }) => {

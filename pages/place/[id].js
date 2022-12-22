ba3c8333a56ff734f63aeb85460fa26657f28370
LEECH <71697577+LEECHANHYUNG@users.noreply.github.com>
2022-12-22 15:45:57 +0900

    Update [id].js

--- pages/place/[id].js
+++ pages/place/[id].js
@@ -159,25 +159,9 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   return {
     paths: [
+      { params: { id: '1' } },
+      { params: { id: '2' } },
       { params: { id: '3' } },
-      { params: { id: '10' } },
-      { params: { id: '11' } },
-      { params: { id: '12' } },
-      { params: { id: '13' } },
-      { params: { id: '14' } },
-      { params: { id: '15' } },
-      { params: { id: '16' } },
-      { params: { id: '17' } },
-      { params: { id: '18' } },
-      { params: { id: '19' } },
-      { params: { id: '20' } },
-      { params: { id: '21' } },
-      { params: { id: '22' } },
-      { params: { id: '23' } },
-      { params: { id: '25' } },
-      { params: { id: '26' } },
-      { params: { id: '26' } },
-      { params: { id: '27' } },
     ],
     fallback: 'blocking',
   };
